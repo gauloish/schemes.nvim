74d717b355ac6772b0fc9d43db9bd47afb602071
gauloish <125079293+gauloish@users.noreply.github.com>
2025-01-03 03:31:04 -0300

    refactor: using the new diagnostic system

--- lua/schemes/parser.lua
+++ lua/schemes/parser.lua
@@ -1,3 +1,5 @@
+local diagnostics = require("schemes.diagnostics")
+
 local M = {}
 
 --- Parse options structure and check if
@@ -10,20 +12,43 @@ M.parse_options = function(options)
 
 	-- Parse options type
 	if type(options) ~= "table" then
-		messages:insert("options must be a \"table\"")
+		messages:insert(diagnostics.create(
+			"options must be a \"table\"",
+			diagnostics.ERROR
+		))
 	else
 		-- Parse options.before type
-		if options.before and type(options.before) ~= "function" then
-			messages:insert("options.before must be \"nil\" or a \"function\"")
+		if options.before then
+			if type(options.before) ~= "function" then
+				messages:insert(diagnostics.create(
+					"options.before must be \"nil\" or a \"function\"",
+					diagnostics.ERROR
+				))
+			else
+				result.before = options.before
+			end
 		else
-			result.before = options.before
+			messages:insert(diagnostics.create(
+				"options.before is \"nil\"",
+				diagnostics.WARNING
+			))
 		end
 
 		-- Parse options.after type
-		if options.after and type(options.after) ~= "function" then
-			messages:insert("options.after must be \"nil\" or a \"function\"")
+		if options.after then
+			if type(options.after) ~= "function" then
+				messages:insert(diagnostics.create(
+					"options.after must be \"nil\" or a \"function\"",
+					diagnostics.ERROR
+				))
+			else
+				result.after = options.after
+			end
 		else
-			result.after = options.after
+			messages:insert(diagnostics.create(
+				"options.before is \"nil\"",
+				diagnostics.WARNING
+			))
 		end
 
 		-- Parse options.default value
@@ -32,30 +57,45 @@ M.parse_options = function(options)
 
 			-- Parse options.default type
 			if type(options.default) ~= "table" then
-				messages:insert("options.default must be either a \"table\"")
+				messages:insert(diagnostics.create(
+					"options.default must be a \"table\"",
+					diagnostics.ERROR
+				))
 				ok = false
 			else
 				-- Parse options.default.name type
 				if type(options.default.name) ~= "string" then
-					messages:insert("options.default.name must be a \"string\"")
+					messages:insert(diagnostics.create(
+						"options.default.name must be a \"string\"",
+						diagnostics.ERROR
+					))
 					ok = false
 				end
 
 				-- Parse options.default.background type
 				if type(options.default.background) ~= "string" then
-					messages:insert("options.default.background must be a \"string\"")
+					messages:insert(diagnostics.create(
+						"options.default.background must be a \"string\"",
+						diagnostics.ERROR
+					))
 					ok = false
 				else
 					-- Parse options.default.background value
 					if options.default.background ~= "dark" and options.default.background ~= "light" then
-						messages:insert("options.default.background value must be \"dark\" or \"light\"")
+						messages:insert(diagnostics.create(
+							"options.default.background value must be \"dark\" or \"light\"",
+							diagnostics.WARNING
+						))
 						ok = false
 					end
 				end
 
 				-- Parse options.default.command value
 				if type(options.default.command) ~= "function" then
-					messages:insert("options.default.command must be a \"function\"")
+					messages:insert(diagnostics.create(
+						"options.default.command must be a \"function\"",
+						diagnostics.ERROR
+					))
 					ok = false
 				end
 			end
@@ -63,6 +103,11 @@ M.parse_options = function(options)
 			if ok then
 				result.default = options.default
 			end
+		else
+			messages:insert(diagnostics.create(
+				"options.default is \"nil\"",
+				diagnostics.WARNING
+			))
 		end
 
 		-- Parse options.schemes value
@@ -76,30 +121,45 @@ M.parse_options = function(options)
 
 					-- Parse options.schemes[key] type
 					if type(scheme) ~= "table" then
-						messages:insert("options.schemes[%s] must be either a \"table\"")
+						messages:insert(diagnostics.create(
+							"options.schemes[%s] must be a \"table\"",
+							diagnostics.ERROR
+						))
 						ok = false
 					else
 						-- Parse options.schemes[key].name type
 						if type(scheme.name) ~= "string" then
-							messages:insert(("options.schemes[%s].name must be a \"string\""):format(key))
+							messages:insert(diagnostics.create(
+								("options.schemes[%s].name must be a \"string\""):format(key),
+								diagnostics.ERROR
+							))
 							ok = false
 						end
 
 						-- Parse options.schemes[key].background type
 						if type(scheme.background) ~= "string" then
-							messages:insert(("options.schemes[%s].background must be a \"string\""):format(key))
+							messages:insert(diagnostics.create(
+								("options.schemes[%s].background must be a \"string\""):format(key),
+								diagnostics.ERROR
+							))
 							ok = false
 						else
 							-- Parse options.schemes[key].background value
 							if scheme.background ~= "dark" and scheme.background ~= "light" then
-								messages:insert(("options.schemes[%s].background value must be \"dark\" or \"light\""):format(key))
+								messages:insert(diagnostics.create(
+									("options.schemes[%s].background value must be \"dark\" or \"light\""):format(key),
+									diagnostics.WARNING
+								))
 								ok = false
 							end
 						end
 
 						-- Parse options.schemes[key].command value
 						if type(scheme.command) ~= "function" then
-							messages:insert(("options.schemes[%s].command must be a \"function\""):format(key))
+							messages:insert(diagnostics.create(
+								("options.schemes[%s].command must be a \"function\""):format(key),
+								diagnostics.ERROR
+							))
 							ok = false
 						end
 					end
@@ -109,10 +169,21 @@ M.parse_options = function(options)
 					end
 				end
 			else
-				messages:insert("options.schemes must be either a \"table\"")
+				messages:insert(diagnostics.create(
+					"options.schemes must be a \"table\""),
+					diagnostics.ERROR
+				))
 			end
+		else
+			messages:insert(diagnostics.create(
+				"options.schemes is \"nil\"",
+				diagnostics.WARNING
+			))
 		end
 	end
 
-	return messages, result
+	return {
+		messages = messages,
+		result = result,
+	}
 end
