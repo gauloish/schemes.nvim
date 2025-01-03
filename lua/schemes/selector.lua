7c8800b7ab92d3e1f382d5b124b6f5194a3663ad
gauloish <125079293+gauloish@users.noreply.github.com>
2025-01-03 04:48:56 -0300

    feat: schemes are listed by telescope picker

---
+++ lua/schemes/selector.lua
@@ -0,0 +1,146 @@
+local diagnostics = require("schemes.diagnostics")
+
+local M = {}
+
+--- File path to save scheme
+M.FILE = vim.fn.stdpath("data") .. "/.scheme"
+M.current = nil
+
+--- Format name and background scheme
+---@param name Name scheme
+---@param background Background scheme
+---@return Formatted string with name and background scheme
+M.format = function(name, background)
+	return ("%s: %s"):format(name, background:gsub("^%l", string.upper))
+end
+
+--- Change current scheme
+---@param scheme Scheme
+---@return Operation status
+M.change = function(scheme)
+	if scheme.background == "light" then
+		vim.opt.background = "light"
+	else
+		vim.opt.background = "dark"
+	end
+
+	if M.current then
+		pcall(M.before, M.current)
+	end
+
+	local ok = pcall(scheme.command)
+
+	if ok then
+		pcall(M.after, scheme)
+	end
+
+	return ok
+end
+
+--- Change and save scheme
+---@param key Scheme key
+M.save = function(key)
+	if key and M.schemes[key] then
+		local file = io.open(FILE, "w")
+
+		if file then
+			if M.change(M.schemes[key]) then
+				file:write(key)
+			end
+
+			file:close()
+		end
+	else
+		pcall(os.remove, FILE)
+		M.change(M.default)
+	end
+end
+
+--- initialize scheme
+M.initialize = function()
+	local file = io.open(FILE, "r")
+	local key = nil
+
+	if file then
+		key = file:read()
+		file:close()
+	end
+	
+	M.save(key)
+end
+
+--- Create a telescope picker for schemes
+---@return The telescope picker for schemes
+M.selector = function()
+	local actions = require("telescope.actions")
+	local pickers = require("telescope.pickers")
+	local finders = require("telescope.finders")
+	local sorters = require("telescope.sorters")
+
+	local keys = {}
+
+	for key, scheme in pairs(M.schemes) do
+		keys:insert(key)
+	end
+
+	local state = require("telescope.actions.state")
+
+	local enter = function(prompt)
+		M.save(state.get_selected_entry()[1])
+		actions.close(prompt)
+	end
+
+	local retreat = function(prompt)
+		actions.move_selection_previous(prompt)
+	end
+
+	local advance = function(prompt)
+		actions.move_selection_next(prompt)
+	end
+
+	local options = {
+		finder = finders.new_table(keys),
+		sorter = sorters.get_fzy_sorter({}),
+		attach_mappings = function(_, map)
+			map("n", "<cr>", enter)
+			map("i", "<cr>", enter)
+
+			map("n", "k", retreat)
+			map("n", "j", advance)
+
+			return true
+		end,
+		layout_config = {
+			width = 50,
+			height = 0.6,
+		},
+		prompt_title = "Schemes",
+	}
+
+	return pickers.new(options)
+end
+
+--- Setup the selector and initialize scheme
+---@param options Options table
+M.setup = function(options)
+	M.before = options.before or function(_) end
+	M.after = options.after or function(_) end
+	M.default = options.default
+	M.schemes = {}
+
+	for _, scheme in pairs(options.schemes) do
+		local key = M.format(scheme.name, scheme.background)
+
+		M.schemes[key] = scheme
+	end
+
+	table.sort(M.schemes)
+
+	vim.api.nvim_create_user_command("Schemes", function()
+		M.selector():find()
+	end, { desc = "Choose colorschemes" })
+
+	M.initialize()
+end
+
+return M
